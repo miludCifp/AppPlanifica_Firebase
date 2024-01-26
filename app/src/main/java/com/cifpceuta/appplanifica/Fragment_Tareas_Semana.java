925143d281d54b646d8efe7d58978f4a9a9ceb80
miludCifp <10395@cifpceuta.es>
2024-01-26 21:06:43 +0100

    Creación de TableLayout de las semanas

--- app/src/main/java/com/cifpceuta/appplanifica/Fragment_Tareas_Semana.java
+++ app/src/main/java/com/cifpceuta/appplanifica/Fragment_Tareas_Semana.java
@@ -56,8 +56,7 @@ public class Fragment_Tareas_Semana extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment__tareas__semana, container, false);
     }
